4753885e1613fb4dd8e093d6c0a5de1702859712
Thenmozhi1111 <thenurajendiran@gmail.com>
2026-06-15 13:41:21 +0530

    Update db.js

--- FileUploadPortal/backend/db.js
+++ FileUploadPortal/backend/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "Fileportal",
-    password: "thenu",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
